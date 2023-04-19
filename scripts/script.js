eb26135d9841e895be44cc4fc1bda2162cc7e57d
K3A101 <keisha.alexander@hva.nl>
2023-04-19 23:57:59 +0200

    feedback dat men gekopieerd is

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,20 @@
 console.log('Hello World!');
 const copyButton = document.querySelector('.copy-button');
 const pasteButton = document.querySelector('.paste-button');
+const copyConfirmation = document.querySelector('.copied-confirmation'); 
 
+console.log(copyConfirmation);
 
 copyButton.addEventListener('click', () => {
     const textCopy = document.querySelector('.text-to-copy').innerText;
     navigator.clipboard.writeText(textCopy)
         .then(() => {
             console.log('Text copied to clipboard');
+            copyConfirmation.innerHTML= 'Paragraaf is gekopieerd';
         })
         .catch((error) => {
             console.log('Text failed to copy');
+            copyConfirmation.innerHTML = 'Paragraaf is niet gekopieerd';
         })
 })
 
@@ -36,4 +40,54 @@ if(SpeechRecognition){
     recognition.lang = 'nl-NL';
     recognition.continuous = true;
     recognition.interimResults = false;
+
+
+const commands = {
+    "ga naar": destination => navigateTo(destination),
+    "ik wil naar": destination => navigateTo(destination)
 }
+
+function navigateTo(destination){
+    console.log('je bent bij deze secties');
+    const destinationLocations = {
+        aardkorst: "#aardkorst",
+        aardmantel: "#aardmantel",
+        aardkern: "#aardkern"
+    };
+    const location = destinationLocations[destination];
+
+    if(location){
+        window.location.href = location;
+    }else {
+        console.log(`Unknown destination: ${destination}`)
+    }
+} 
+
+function onSpeechRecognitionEvents() {
+    recognition.addEventListener('result', event =>{
+        if(typeof event.results === 'undefined') return;
+        const transcript = event.results[event.results.length - 1][0].transcript
+        .toLowerCase()
+        .trim();
+        for(let command in commands){
+            if(transcript.indexOf(command)=== 0){
+                if (transcript[command.length] === undefined) {
+                    commands[command]();
+                } else {
+                    const param = transcript
+                        .substring(command.length, transcript.length)
+                        .trim();
+                    commands[command](param);
+                } 
+            }
+        }
+});
+}
+
+if (SpeechRecognition) {
+    onSpeechRecognitionEvents();
+    recognition.start();
+}
+}
+
+// ----------------------------
