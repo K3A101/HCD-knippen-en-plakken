a5c783e1ea77481c4ea47e4e87acaf422df394ee
K3A101 <keisha.alexander@hva.nl>
2023-04-20 21:53:07 +0200

    code refactor

--- scripts/script.js
+++ scripts/script.js
@@ -1,93 +1,92 @@
-console.log('Hello World!');
-const copyButton = document.querySelector('.copy-button');
-const pasteButton = document.querySelector('.paste-button');
-const copyConfirmation = document.querySelector('.copied-confirmation'); 
 
-console.log(copyConfirmation);
+const copyButtons = document.querySelectorAll('.copy-button');
+const copyConfirmations = document.querySelectorAll('.copied-confirmation');
+const textToCopy = document.querySelectorAll('.text-to-copy');
+const pasteButtons = document.querySelectorAll('.paste-button');
+const textAreas = document.querySelectorAll('textarea');
+for (let i = 0; i < pasteButtons.length; i++) {
+    pasteButtons[i].classList.add('hidden');
+    
+}
 
-copyButton.addEventListener('click', () => {
-    const textCopy = document.querySelector('.text-to-copy').innerText;
-    navigator.clipboard.writeText(textCopy)
-        .then(() => {
-            console.log('Text copied to clipboard');
-            copyConfirmation.innerHTML= 'Paragraaf is gekopieerd';
-        })
-        .catch((error) => {
-            console.log('Text failed to copy');
-            copyConfirmation.innerHTML = 'Paragraaf is niet gekopieerd';
-        })
-})
+let copiedText
 
-pasteButton.addEventListener('click', () => {
-    navigator.clipboard.readText()
-        .then((textCopy) => {
-            if (textCopy) {
-                document.getElementById('paste-input').value = textCopy;
-            } else {
-                console.warn("Clipboard is empty or does not contain text.");
-            }
+for (let i = 0; i < copyButtons.length; i++) {
+    copyButtons[i].addEventListener("click", () => {
+        navigator.clipboard.writeText(textToCopy[i].innerText);
+        copiedText = textToCopy[i].innerText
+        console.log('Tekst gekopieerd');
+        copyConfirmations[i].innerHTML = 'Paragraaf is gekopieerd';
+        pasteButtons[i].classList.remove('hidden');
+        
+    })
 
-        })
-        .catch((error) => {
-            console.log('Text failed to paste');
-        })
-})
 
+    pasteButtons[i].addEventListener('click', () => {
+        textAreas[i].value = copiedText;
+        copiedText = ""
+        console.log('Tekst is geplakt')
+        pasteButtons[i].classList.add('hidden');
+
+    })
+
+
+}
 // ----------------------------
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-if(SpeechRecognition){
+if (SpeechRecognition) {
     const recognition = new SpeechRecognition();
     recognition.lang = 'nl-NL';
     recognition.continuous = true;
     recognition.interimResults = false;
 
 
-const commands = {
-    "ga naar": destination => navigateTo(destination),
-    "ik wil naar": destination => navigateTo(destination)
-}
-
-function navigateTo(destination){
-    console.log('je bent bij deze secties');
-    const destinationLocations = {
-        aardkorst: "#aardkorst",
-        aardmantel: "#aardmantel",
-        aardkern: "#aardkern"
-    };
-    const location = destinationLocations[destination];
-
-    if(location){
-        window.location.href = location;
-    }else {
-        console.log(`Unknown destination: ${destination}`)
+    const commands = {
+        "ga naar": destination => navigateTo(destination),
+        "ik wil naar": destination => navigateTo(destination)
     }
-} 
 
-function onSpeechRecognitionEvents() {
-    recognition.addEventListener('result', event =>{
-        if(typeof event.results === 'undefined') return;
-        const transcript = event.results[event.results.length - 1][0].transcript
-        .toLowerCase()
-        .trim();
-        for(let command in commands){
-            if(transcript.indexOf(command)=== 0){
-                if (transcript[command.length] === undefined) {
-                    commands[command]();
-                } else {
-                    const param = transcript
-                        .substring(command.length, transcript.length)
-                        .trim();
-                    commands[command](param);
-                } 
-            }
+    function navigateTo(destination) {
+        console.log('je bent bij deze secties');
+        const destinationLocations = {
+            aardkorst: "#aardkorst",
+            aardmantel: "#aardmantel",
+            aardkern: "#aardkern"
+        };
+        const location = destinationLocations[destination];
+
+        if (location) {
+            window.location.href = location;
+        } else {
+            console.log(`Unknown destination: ${destination}`)
         }
-});
-}
+    }
 
-if (SpeechRecognition) {
-    onSpeechRecognitionEvents();
-    recognition.start();
-}
+    function onSpeechRecognitionEvents() {
+        recognition.addEventListener('result', event => {
+            if (typeof event.results === 'undefined') return;
+            const transcript = event.results[event.results.length - 1][0].transcript
+                .toLowerCase()
+                .trim();
+            for (let command in commands) {
+                if (transcript.indexOf(command) === 0) {
+                    if (transcript[command.length] === undefined) {
+                        commands[command]();
+                    } else {
+                        const param = transcript
+                            .substring(command.length, transcript.length)
+                            .trim();
+                        commands[command](param);
+                    }
+                }
+            }
+        });
+    }
+
+    if (SpeechRecognition) {
+        onSpeechRecognitionEvents();
+        recognition.start();
+    }
 }
 
 // ----------------------------
